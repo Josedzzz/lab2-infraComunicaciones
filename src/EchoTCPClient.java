4ac72f9cb3807c17e4435bea1b3e708dee8a6341
dxniel.hit <dancorrea698@gmail.com>
2024-08-21 21:21:28 -0500

    Clases Cliente y Servidor.

---
+++ src/EchoTCPClient.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoTCPClient
+{
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static final String server = "localhost";
+    public static final int PORT = 3400;
+
+    private PrintWriter toNetwork;
+    private BufferedReader fromNetwork;
+
+    private Socket clientsideSocket;
+
+    public EchoTCPClient() {
+        System.out.println("Echo TCP de cliente corriendo...");
+    }
+
+    public void init() throws Exception {
+
+        clientsideSocket = new Socket(server, PORT);
+        createStream(clientsideSocket);
+        protocol(clientsideSocket);
+
+        clientsideSocket.close();
+    }
+
+    public void protocol(Socket socket) throws Exception {
+        System.out.print("Tipo de conversión: ");
+        String input = scanner.nextLine();
+        toNetwork.println(input);
+
+        String fromServer = fromNetwork.readLine();
+        System.out.println("[Cliente] Desde servidor: " + fromServer);
+    }
+
+    private void createStream(Socket socket) throws Exception {
+        toNetwork = new PrintWriter(socket.getOutputStream(), true);
+        fromNetwork = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public static void main(String[] args) throws Exception {
+        EchoTCPClient client = new EchoTCPClient();
+        client.init();
+    }
+
+
+}
---
+++ src/EchoTCPServer.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoTCPServer {
+
+    public static final int PORT = 3400;
+
+    private ServerSocket listener;
+    private Socket serversideSocket;
+
+    private PrintWriter toNetwork;
+    private BufferedReader fromNetwork;
+
+    public EchoTCPServer() {
+        System.out.println("El servidor con eco TCP está corriendo en el puerto: " + PORT);
+    }
+
+    public void init() throws Exception {
+
+        listener = new ServerSocket(PORT);
+
+        while (true) {
+            serversideSocket = listener.accept();
+
+            createStreams(serversideSocket);
+            protocol(serversideSocket);
+        }
+    }
+
+    public void protocol(Socket socket) throws Exception {
+        String message = fromNetwork.readLine();
+        System.out.println("[Server] desde cliente: " + message);
+        String answer = message; // Que redundante
+        toNetwork.println(answer);
+    }
+
+    public void createStreams(Socket socket) throws Exception {
+        toNetwork = new PrintWriter(socket.getOutputStream(), true);
+        fromNetwork = new BufferedReader(new InputStreamReader(serversideSocket.getInputStream()));
+    }
+
+    public static void main(String[] args) throws Exception {
+        EchoTCPServer server = new EchoTCPServer();
+        server.init();
+    }
+}
